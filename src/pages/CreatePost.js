34055e1d7710459ff20db52917ec90a58fd47d52
Tao Huang <thuang@unc.edu>
2022-11-06 13:36:22 -0500

    update create post background color

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -59,7 +59,7 @@ const CreatePost = ({ isAuth }) => {
   }, []);
 
   return (
-    <div className="ceatePostPate">
+    <div className="createPostPage">
       <div className="cpContainer">
         <h1>Post Your Items</h1>
         <div className="inputGp">
